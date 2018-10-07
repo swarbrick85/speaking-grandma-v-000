d96b3b1d541ffea1018141f2fee79559a08f60a4
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-07 17:25:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,4 +2,7 @@ def speak_to_grandma(speech)
   if speech !== speech.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif speech == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif speech == speech.upcase
+   return "NO, NOT SINCE 1938!"
+ end
